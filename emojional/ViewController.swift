9c7f5b35e92510006b1b7c83ad4a7a41d9829c56
Apple <apple@Apples-MacBook-Pro-101.local>
2019-06-21 11:47:36 -0400

    complete iteration 3

--- emojional/ViewController.swift
+++ emojional/ViewController.swift
@@ -9,19 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBAction func showMessage(sender : UIButton) {
-        let alertController = UIAlertController(title: "Mood", message: "", preferredStyle: UIAlertController.Style.alert)
-      alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
     
-        present(alertController, animated: true, completion: nil)
-    }
+    let 
     
+        var emojis = ["🤯": ["You can do it", "Stop your stressing", "Take a breather"], "🤠": ["Howdy", "Giddy up", "keep on ridin"], "🥳": ["Time to party", "You have officially accomplished in life", "Good Job!!"]]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+//    @IBAction func showMessage(sender: UIButton){
+//    }
+    @IBAction func showMessage(sender: UIButton) {
+        let selectedEmotion = sender.titleLabel?.text
+        
+        // the rest of your code from Iteration 1
+    
+        let alertController = UIAlertController(title: "Mood", message: "hello", preferredStyle: UIAlertController.Style.alert)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+    
+        present(alertController, animated: true, completion: nil)
+    
+    }
 }
-
